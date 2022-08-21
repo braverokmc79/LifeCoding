3edbe6e0513361c9fabfeb62058d5e34c5f870cd
braverokmc79 <braverokmc79@gmail.com>
2022-08-22 04:38:09 +0900

    LifeCoding2  4. 함수에서 state 사용법 hook

--- react-app2/src/App.js
+++ react-app2/src/App.js
@@ -1,5 +1,5 @@
 
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -14,10 +14,43 @@ function App() {
 }
 
 function FuncComp(props) {
+
+
+  // let numberState = useState(props.initNumber);
+  // let number = numberState[0];
+  // let setNumber = numberState[1];
+
+  // let dateState = useState((new Date()).toString());
+  // let _date = dateState[0];
+  // let setDate = dateState[1];
+
+
+  const [number, setNumber] = useState([]);
+  useEffect(() => {
+    setNumber(props.initNumber)
+  }, []);
+
+  const [_date, setDate] = useState((new Date()).toString());
+  
+
+
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+
+      <input type="button" value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }} />
+
+      <input type="button" value="date"
+        onClick={function () {
+          setDate((new Date()).toString());
+        }} />
+
+
     </div>
   );
 }
@@ -26,7 +59,8 @@ function FuncComp(props) {
 
 class ClassComp extends Component {
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
 
   render() {
@@ -34,9 +68,15 @@ class ClassComp extends Component {
       <div className='container'>
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random"
           onClick={function () {
             this.setState({ number: Math.random() })
+          }.bind(this)} />
+
+        <input type="button" value="date"
+          onClick={function () {
+            this.setState({ date: (new Date()).toString() })
           }.bind(this)} />
 
       </div >
