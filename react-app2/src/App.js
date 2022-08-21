ad16f9c74fed639001694004193c9d94cf5f9d04
braverokmc79 <braverokmc79@gmail.com>
2022-08-22 04:14:15 +0900

    LifeCoding2  3.클래스에서 state 사용법

--- react-app2/src/App.js
+++ react-app2/src/App.js
@@ -7,27 +7,39 @@ function App() {
     <div className="container">
       <h1>Hello World</h1>
 
-      <FuncComp></FuncComp>
-      <ClassComp></ClassComp>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
     </div>
   );
 }
 
-function FuncComp() {
+function FuncComp(props) {
   return (
     <div className="container">
       <h2>function style component</h2>
+      <p>Number : {props.initNumber}</p>
     </div>
   );
 }
 
+
+
 class ClassComp extends Component {
+  state = {
+    number: this.props.initNumber
+  }
 
   render() {
     return (
       <div className='container'>
         <h2>class style component</h2>
-      </div>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() })
+          }.bind(this)} />
+
+      </div >
     )
   }
 }
